c3a11ffd8751ab881c6c94c2ec4e9ac078a5aebf
Krishiv1611 <krishivarora150@gmail.com>
2025-06-21 17:51:08 +0530

    api chnages

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,38 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
 const cors = require("cors");
 
 const app = express();
 
-const { userModel, taskModel } = require("./database");
 const { userRouter } = require("./routes/user");
 const { taskRouter } = require("./routes/task");
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const PORT = process.env.PORT || 8000;
+
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: process.env.FRONT_END, // Use FRONT_END from .env for CORS
   credentials: true
 }));
+
 app.use(express.json());
+
 app.use("/users", userRouter);
 app.use("/tasks", taskRouter);
-async function main() {
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("MongoDB connected");
 
-        app.listen(process.env.PORT||8000, () => {
-            console.log("Server listening on port 8000");
-        });
-    } catch (err) {
-        console.error("Error connecting to MongoDB:", err);
-    }
+async function main() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  }
 }
 
 main();
+
 
